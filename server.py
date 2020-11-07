c0a2aeadceb86788526efab04cbf72a49edb97c1
Wojciech Wesołowski <wojtek.wes@gmail.com>
2020-11-07 15:42:05 +0100

    Initial commit

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
 # put your code here
+from flask import Flask, render_template, request
+import requests
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    url = request.args.get('url', 'https://swapi.dev/api/planets')
+    response = requests.get(url)
+    obj = response.json()
+
+    return render_template('index.html', data=obj)
+
+
+if __name__ == '__main__':
+    app.run()
